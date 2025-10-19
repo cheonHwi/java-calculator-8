31d1b8cbf86d220abc7111290e62802130e94e7c
cheonHwi <hoba0409@naver.com>
2025-10-20 03:20:52 +0900

    test(Application): 커스텀 구분자 추가 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         String delimiter = "[,:]";
@@ -8,6 +10,21 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = InputConsole.readLine();
         if(input.isBlank()) return;
+
+        if (input.startsWith("//")) {
+            int delimiterEnd = input.indexOf("\\n");
+            if (delimiterEnd == -1) {
+                throw new IllegalArgumentException("Invalid Delimiter format");
+            }
+
+            String customDelimiter = input.substring(2, delimiterEnd);
+            if(customDelimiter.length() != 1)
+                throw new IllegalArgumentException("Custom Delimiter length must be 1");
+
+            delimiter = "[,:|" + Pattern.quote(customDelimiter) + "]";
+            input = input.substring(delimiterEnd + 2);
+        }
+
         String[] tokenList = input.split(delimiter);
 
         for (String token : tokenList) {
