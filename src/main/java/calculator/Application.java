faff0ab00348199ed1b2f34772b11d76c8e65f3c
cheonHwi <hoba0409@naver.com>
2025-10-20 02:56:06 +0900

    feat(Application): 기본 구분자 덧셈 구현
    - 입력값 검증
    - 0 이하의 값 입력시 예외 발생

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,26 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
-        public static void main(String[] args) {
-            String delimiter = "[,:]";
+    public static void main(String[] args) {
+        String delimiter = "[,:]";
+        int result = 0;
 
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String input = Console.readLine();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = InputConsole.readLine();
+        if(input.isBlank()) return;
+        String[] tokenList = input.split(delimiter);
 
-            String[] tokenList = input.split(delimiter);
-
-
-            for (String token : tokenList) {
-                System.out.println(token);
+        for (String token : tokenList) {
+            try {
+                int parsedToken = Integer.parseInt(token);
+                if(parsedToken < 0) throw new NumberFormatException();
+                else result += parsedToken;
             }
+            catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Invalid Input data");
+            }
+        }
+
+        System.out.println("결과 : " + result);
         }
 }
